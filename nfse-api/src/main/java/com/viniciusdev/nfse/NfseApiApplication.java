da94910e53b4c1fc8e0b8bb5fab4dbd307537301
Morcego <vinicius.templario@gmail.com>
2023-06-04 09:42:44 -0300

    Adicionando o workspace do projeto

---
+++ nfse-api/src/main/java/com/viniciusdev/nfse/NfseApiApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusdev.nfse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfseApiApplication.class, args);
+	}
+
+}
